77770f0ac85540f5139318d1b8807766ce69ab0d
Chehui Chou <deadshot465@users.noreply.github.com>
2020-08-07 04:07:28 +0900

    Bug fixes.

--- owoify/__init__.py
+++ owoify/__init__.py
@@ -1,5 +1,3 @@
 __version__ = '1.0.0'
 
 from .owoify import owoify
-import owoify.structures
-import owoify.utility
---
+++ owoify/utility/__init__.py
@@ -0,0 +1,3 @@
+from .interleave_arrays import interleave_arrays
+from .mapping import *
+from .presets import *
