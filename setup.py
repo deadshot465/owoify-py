d9a0d46b481348c3a153e6cb568adcda8b984889
Chehui Chou <deadshot465@users.noreply.github.com>
2020-08-15 13:44:39 +0900

    Update setup.py.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description='Turning your worst nightmare into a Python package.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/deadshot465/owoify.py',
+    url='https://github.com/deadshot465/owoify-py',
     packages=setuptools.find_packages(),
     classfiers=[
         'Programming Language :: Python :: 3.6',
